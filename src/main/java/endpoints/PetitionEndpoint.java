4bd06dc92f0ed870ded349056fb101a447e2e527
PierreBizos <pierre.bizos@etu.univ-nantes.fr>
2019-06-17 13:31:50 +0200

    ajout vote

--- src/main/java/endpoints/PetitionEndpoint.java
+++ src/main/java/endpoints/PetitionEndpoint.java
@@ -2,6 +2,7 @@ package endpoints;
 import com.google.api.client.util.store.DataStore;
 import com.google.api.server.spi.auth.common.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -21,6 +22,7 @@ import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Transaction;
 import com.google.appengine.api.datastore.Query.*;
 
 
@@ -37,14 +39,22 @@ public class PetitionEndpoint {
 	//Ajouter une pétition
 	@ApiMethod(name = "addPetition")
 	public Entity addPetition(@Named("mailAuteurPetition") String mailAuteurPetition, @Named("titrePetition") String titrePetition, @Named("descriptionPetition") String descriptionPetition) {
-			Entity e = new Entity("Petition", "P_"+titrePetition);
-			e.setProperty("mailAuteurPetition", mailAuteurPetition);
-			e.setProperty("titrePetition", titrePetition);
-			e.setProperty("descriptionPetition", descriptionPetition);
-			e.setProperty("nbSignature", 0);
+			Entity petition = new Entity("Petition", "P_"+titrePetition);
+			petition.setProperty("mailAuteurPetition", mailAuteurPetition);
+			petition.setProperty("titrePetition", titrePetition);
+			petition.setProperty("descriptionPetition", descriptionPetition);
+			petition.setProperty("nbSignature", 0);
+			petition.setProperty("lastIndexOfSignature", titrePetition+"V_1");
+			
+			List arrayListSignature = new ArrayList();
+			Entity listSignature = new Entity("Vote", petition.getKey());
+			listSignature.setProperty("listSignature", arrayListSignature);
+			listSignature.setIndexedProperty("indiceListeVote", titrePetition+"V_1");	
+			
 			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-			datastore.put(e);
-			return  e;
+			datastore.put(petition);
+			datastore.put(listSignature);
+			return  petition;
 	}
 
 	/* Lister toutes les pétitions, remplacé par la liste des pétitions triées
@@ -88,13 +98,59 @@ public class PetitionEndpoint {
 	//Ajouter un vote avec une pétition en paramètre
 	// /!\ En construction /!\
 	@ApiMethod(name = "addVote")
-	public Entity addVote(@Named("petitionParent") String petitionParent) {
-		
-			Entity e = new Entity("Vote", petitionParent);
-						
+	public Entity addVote(@Named("petitionParent") String petitionParent, @Named("emailVotant") String emailVotant, @Named("nomVotant") String nomVotant) {
+			
+			boolean newListe = false;
+			String chaine ="";
+			Query reqPetition = new Query("Petition")
+			        .setFilter(new FilterPredicate("titrePetition", FilterOperator.EQUAL, petitionParent));
+			
 			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-			datastore.put(e);
-
-			return  e;	
+			PreparedQuery pq = datastore.prepare(reqPetition);
+			Entity result = pq.asSingleEntity();
+			
+			Query reqVote = new Query("Vote")
+			        .setFilter(new FilterPredicate("indiceListeVote", FilterOperator.EQUAL, result.getProperty("lastIndexOfSignature")));
+			PreparedQuery prepareVote = datastore.prepare(reqVote);
+			Entity res = prepareVote.asSingleEntity();
+			ArrayList arraySign;
+			if((ArrayList)res.getProperty("listSignature") != null) {
+				arraySign = (ArrayList)res.getProperty("listSignature");
+			}else {
+				arraySign = new ArrayList();
+			}
+			if(arraySign.size() >= 20000) {
+				newListe = true;
+				Entity listSignature = new Entity("Vote", result.getKey());
+				ArrayList arraySignNew = new ArrayList();
+				arraySignNew.add(emailVotant);
+				listSignature.setProperty("listSignature", arraySignNew);
+				chaine = (String)result.getProperty("lastIndexOfSignature");
+				chaine = chaine.substring(chaine.length()-1);
+				
+				chaine = petitionParent+"V_"+(Integer.parseInt(chaine)+1);
+				listSignature.setIndexedProperty("indiceListeVote", chaine);	
+				datastore.put(listSignature);
+			}else {
+				arraySign.add(emailVotant);
+				res.setProperty("listSignature", arraySign);
+				datastore.put(res);
+			}
+			
+			Transaction transNbSign = datastore.beginTransaction();
+			try {
+				result.setProperty("nbSignature", (long)result.getProperty("nbSignature")+1);
+				if(newListe) {
+					result.setProperty("lastIndexOfSignature", chaine);
+				}
+				datastore.put(transNbSign,result);
+				transNbSign.commit();
+			}finally {
+			    if (transNbSign.isActive()) {
+			    	transNbSign.rollback();
+			      }
+		    }
+			return res;
 	}
+	
 }
